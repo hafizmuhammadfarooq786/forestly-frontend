5df33a4e99a80c30b7afdf3ba1a45206d634ac59
Hafiz Muhammad Farooq <farooq@we-over-i.com>
2021-03-21 19:33:00 +0500

    updated env variables

--- src/services/api/ApiService.js
+++ src/services/api/ApiService.js
@@ -6,7 +6,7 @@ import OpenAPIClientAxios from "openapi-client-axios";
 import Schema from "./openapi-schema";
 
 const openApi = new OpenAPIClientAxios({ definition: Schema });
-const API_URL = process.env.REACT_APP_BASE_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 /**
  *
  * @type
